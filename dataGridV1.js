d528ef898dfd00151696a00171a76633be78f766
sofienkhalifa01 <69243160+sofienkhalifa01@users.noreply.github.com>
2021-04-01 11:48:34 +0100

    Create tabgrid

---
+++ dataGridV1.js
@@ -0,0 +1,309 @@
+const dataTab = [
+    {
+        id: 1,
+        classnames: 'course one-of-four text-2ebd59 MDev',
+        imgsrc: 'images/courses/courses-gird-v1-1.png',
+        title: 'Iphone App Development Course',
+        Overview: 'This iPhone and iPad app development course is designed to get you started in iPhone app development. This course includes Interface Builder, Xcode, Swift, Cocoa Touch programming, and the APIs included in the iOS SDK',
+        DESCRIPTION1: 'Training from professional Android application developers ',
+        DESCRIPTION2:'Real-time Practice and Projects ',
+        DESCRIPTION3:'Course Completion Certificate',
+        DESCRIPTION4:'Support and Careers Advice',
+        plan1:'Introduction Advanced iOS Development course',
+        plan2:'Swift Basics',
+        plan3:'Cocoa Touch Design Patterns  ',
+        plan4:'Foundation',
+        plan5:'Xcode IDE as a development tool',
+        plan6:'View and View Controllers',
+        plan7:'Interface builder GUI Tool ',
+        plan8:'Table Views ',
+        plan9:'Graphics',
+        plan10:'Gesture Recognizers        ',
+        plan11:'Animation',
+        plan12:'System Events',
+        plan13:'iPhone and iPad virtual keyboard',
+        plan14:'iPad App Development ',
+        plan15:'Performance and Debugging',
+        plan16:'Manage Navigation ',
+        plan17:'Integrate with Apple APIs ',
+        plan18:'Integration with 3rd Party APIs',
+        plan19:'Concurrency',
+        plan20:'Submitting your app for approval',
+        Requirements:'',
+        count: '5.0 (980)',
+        price: 'FREE'
+    },
+    {
+        id: 2,
+        classnames: 'course one-of-four text-ea0042 MDev ',
+        imgsrc: 'images/courses/courses-gird-v1-2.jpg',  
+        title: 'Android Programming Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+    {
+        id: 3,
+        classnames: 'course one-of-four text-c100ea MDev',
+        imgsrc: 'images/courses/courses-gird-v1-3.jpg',
+        title: 'iPad app development Course',
+        count: '5.0 (980)',
+        price: '$24.00'
+    },
+    {
+        id: 4,
+        classnames: 'course one-of-four text-256cff MDev',
+        imgsrc: 'images/courses/c.png',
+        title: 'Introduction to Objective-C Training',
+        count: '5.0 (980)',
+        price: '$89.00'
+    },
+    {
+        id: 5,
+        classnames: 'course one-of-four text-8828ff MDev',
+        imgsrc: 'images/courses/swift.jpg',
+        title: 'Swift Course',
+        count: '5.0 (980)',
+        price: '$24.00'
+    },
+    {
+        id: 6,
+        classnames: 'course one-of-four text-0dacff MDev',
+        imgsrc: 'images/courses/courses-gird-v1-6.jpg',
+        title: 'Advanced Android App Development Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+    {
+        id: 7,
+        classnames: 'course one-of-four text-536dfe MDev',
+        imgsrc: 'images/courses/courses-gird-v1-7.jpg',
+        title: 'BlackBerry Application Development Course',
+        count: '5.0 (980)',
+        price: '$24.00'
+    },
+    {
+        id: 8,
+        classnames: 'course one-of-four text-ffbf13 MDev',
+        imgsrc: 'images/courses/courses-gird-v1-8.jpg',
+        title: 'PhoneGap Training',
+        count: '5.0 (980)',
+        price: '$89.00'
+    },
+    {
+        id: 9,
+        classnames: 'course one-of-four text-2ebd59 MDev NET',
+        imgsrc: 'images/background1.png',
+        title: 'Xamarin Course',
+        count: '5.0 (980)',
+        price: 'FREE'
+    },
+    {
+        id: 10,
+        classnames: 'course one-of-four text-ff562f MDev',
+        imgsrc: 'images/background2.png',
+        title: 'Ionic Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+    {
+        id: 11,
+        classnames: 'course one-of-four text-0dacff MDev',
+        imgsrc: 'images/background3.png',
+        title: 'Introduction to Programming Course',
+        count: '5.0 (980)',
+        price: '$24.00'
+    },
+    {
+        id: 12,
+        classnames: 'course one-of-four text-ff562f SDev',
+        imgsrc: 'images/background4.png',
+        title: 'Game Development Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 13,
+        classnames: 'course one-of-four text-ff562f SDev',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'C Course',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 14,
+        classnames: 'course one-of-four text-ff562f SDev',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'C++ Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 15,
+        classnames: 'course one-of-four text-ff562f SDev NET',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'VB.NET Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 16,
+        classnames: 'course one-of-four text-ff562f SDev NET',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Windows Forms using C# Course',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 17,
+        classnames: 'course one-of-four text-ff562f SDev NET',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'WPF using C# Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 18,
+        classnames: 'course one-of-four text-ff562f SDev',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Java SE7 Fundamentals Course',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 19,
+        classnames: 'course one-of-four text-ff562f SDev',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'OCA Java SE 8 Programmer Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 20,
+        classnames: 'course one-of-four text-ff562f SDev',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'OCP Java SE 8 Programmer Course',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 21,
+        classnames: 'course one-of-four text-ff562f SDev',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Java SE7 Programming Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 22,
+        classnames: 'course one-of-four text-ff562f Pyt',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Python for beginners Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 23,
+        classnames: 'course one-of-four text-ff562f Pyt',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Python Course',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 24,
+        classnames: 'course one-of-four text-ff562f Pyt',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Advanced Python Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 26,
+        classnames: 'course one-of-four text-ff562f Pyt',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Data Science using Python Course',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },{
+        id: 27,
+        classnames: 'course one-of-four text-ff562f Pyt',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Data Analysis using Python Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+    {
+        id: 28,
+        classnames: 'course one-of-four text-ff562f Pyt',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Machine Learning using Python Course',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+    {
+        id: 29,
+        classnames: 'course one-of-four text-ff562f Pyt',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Deep Learning using Python Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+    {
+        id: 30,
+        classnames: 'course one-of-four text-ff562f Pyt',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Django Course',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+    {
+        id: 31,
+        classnames: 'course one-of-four text-ff562f DSA',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Data Science using Python Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+    {
+        id: 32,
+        classnames: 'course one-of-four text-ff562f DSA',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Data Analysis using Python Course',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+    {
+        id: 33,
+        classnames: 'course one-of-four text-ff562f DSA',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Machine Learning using Python Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+    {
+        id: 34,
+        classnames: 'course one-of-four text-ff562f DSA',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'Deep Learning using Python Course',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+    {
+        id: 35,
+        classnames: 'course one-of-four text-ff562f NET',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'MTA Software Development Fundamentals with .NET Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+    {
+        id: 36,
+        classnames: 'course one-of-four text-ff562f NET',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'ASP.NET Course',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+    {
+        id: 37,
+        classnames: 'course one-of-four text-ff562f NET',
+        imgsrc: 'images/courses/courses-gird-v1-10.jpg',
+        title: 'ASP.NET MVC Training',
+        count: '5.0 (980)',
+        price: '$49.00'
+    },
+                                        
+
+    
+    
+]
+
+
+export default dataTab;
