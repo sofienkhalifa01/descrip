159c63302a084d1c5e87f10dd2234fba8453721f
sofienkhalifa01 <69243160+sofienkhalifa01@users.noreply.github.com>
2021-04-03 17:30:01 +0100

    Update dataGridV2.js

--- dataGridV2.js
+++ dataGridV2.js
@@ -1,13 +1,22 @@
 const dataTab = [
     {
         id: 1,
-        classnames: 'course style2 one-of-three text-005eea Marketing Design',
-        imgsrc: 'images/courses/courses-gird-v2-1.jpg',
-        title: 'Programming Techniques',
-        teacher: 'Ana Murphy',
-        description: 'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.',
+        classnames: 'course one-of-four text-c100ea MDev',
+        imgsrc: 'images/courses/courses-gird-v1-3.jpg',
+        title: 'iPad app development Course',
+        overview: 'This iPad app development course is designed to discuss advanced iPad app development. After completing our iPad app training course you would be able to build applications for iPad. ',
+        DESCRIPTION1: 'Training from professional iPhone and iPad application developers ',
+        DESCRIPTION2:'Real-time Practice and Projects ',
+        DESCRIPTION3:'Course Completion Certificate',
+        DESCRIPTION4:'Support and Careers Advice',
+        plan1:'Introduction to the iPad course',
+        plan2:'Apple’s Built-in View Controllers for iPad',
+        plan3:'Designing your own View Controllers for iPad ',
+        plan4:'NewsStand Kit in iOS 5',
+        plan5:'iPad’s unique features',
+        Requirements:'You should have some experience with iPhone App development.',
         count: '5.0 (980)',
-        price: 'FREE'
+        price: '$24.00'
     },
     {
         id: 2,
