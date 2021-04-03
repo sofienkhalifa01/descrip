2d32500f59075568aca9552acfd0cc755d464fda
sofienkhalifa01 <69243160+sofienkhalifa01@users.noreply.github.com>
2021-04-03 17:30:33 +0100

    Update dataGridV2.js

--- dataGridV2.js
+++ dataGridV2.js
@@ -20,54 +20,42 @@ const dataTab = [
     },
     {
         id: 2,
-        classnames: 'course style2 one-of-three text-ea0042 Marketing ',
-        imgsrc: 'images/courses/courses-gird-v2-2.jpg',
-        title: 'Complete JavaScript Course',
-        teacher: 'Rosy Janner',
-        description: 'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.',
-        count: '4.7  (2,632)',
-        price: '$49.00'
+        classnames: 'course one-of-four text-c100ea MDev',
+        imgsrc: 'images/courses/courses-gird-v1-3.jpg',
+        title: 'iPad app development Course',
+        overview: 'This iPad app development course is designed to discuss advanced iPad app development. After completing our iPad app training course you would be able to build applications for iPad. ',
+        DESCRIPTION1: 'Training from professional iPhone and iPad application developers ',
+        DESCRIPTION2:'Real-time Practice and Projects ',
+        DESCRIPTION3:'Course Completion Certificate',
+        DESCRIPTION4:'Support and Careers Advice',
+        plan1:'Introduction to the iPad course',
+        plan2:'Apple’s Built-in View Controllers for iPad',
+        plan3:'Designing your own View Controllers for iPad ',
+        plan4:'NewsStand Kit in iOS 5',
+        plan5:'iPad’s unique features',
+        Requirements:'You should have some experience with iPhone App development.',
+        count: '5.0 (980)',
+        price: '$24.00'
     },
     {
         id: 3,
-        classnames: 'course style2 one-of-three text-00dcea Marketing Design',
-        imgsrc: 'images/courses/courses-gird-v2-3.jpg',
-        title: 'Learning Python Data Analysis',
-        teacher: 'Tom Steven',
-        description: 'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.',
-        count: '4.8  (7,982)',
-        price: '$89.00'
-    },
-    {
-        id: 4,
-        classnames: 'course style2 one-of-three text-005eea Business',
-        imgsrc: 'images/courses/courses-gird-v2-4.jpg',
-        title: 'jQuery Mobile for Beginners',
-        teacher: 'Ana Murphy',
-        description: 'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.',
+        classnames: 'course one-of-four text-c100ea MDev',
+        imgsrc: 'images/courses/courses-gird-v1-3.jpg',
+        title: 'iPad app development Course',
+        overview: 'This iPad app development course is designed to discuss advanced iPad app development. After completing our iPad app training course you would be able to build applications for iPad. ',
+        DESCRIPTION1: 'Training from professional iPhone and iPad application developers ',
+        DESCRIPTION2:'Real-time Practice and Projects ',
+        DESCRIPTION3:'Course Completion Certificate',
+        DESCRIPTION4:'Support and Careers Advice',
+        plan1:'Introduction to the iPad course',
+        plan2:'Apple’s Built-in View Controllers for iPad',
+        plan3:'Designing your own View Controllers for iPad ',
+        plan4:'NewsStand Kit in iOS 5',
+        plan5:'iPad’s unique features',
+        Requirements:'You should have some experience with iPhone App development.',
         count: '5.0 (980)',
-        price: 'FREE'
+        price: '$24.00'
     },
-    {
-        id: 5,
-        classnames: 'course style2 one-of-three text-ea0042 Design',
-        imgsrc: 'images/courses/courses-gird-v2-5.jpg',
-        title: 'Web Technology',
-        teacher: 'Rosy Janner',
-        description: 'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.',
-        count: '4.7  (2,632)',
-        price: '$49.00'
-    },
-    {
-        id: 6,
-        classnames: 'course style2 one-of-three text-00dcea Leadership',
-        imgsrc: 'images/courses/courses-gird-v2-6.jpg',
-        title: 'Special Education Teaching',
-        teacher: 'Rosy Janner',
-        description: 'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.',
-        count: '4.8  (7,982)',
-        price: '$89.00'
-    }
 ]
 
 export default dataTab;
